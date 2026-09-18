a987409146e410ced68bcb891619ff619947e355
nitroxdiver0815 <nitroxdiver0815@gmail.com>
2026-09-18 21:44:14 +0200

    Füge ArcGIS-Konfigurationsvorlage hinzu

---
+++ public/config.example.js
@@ -0,0 +1,30 @@
+window.LAGETRAINING_ADMIN_CONFIG = {
+  portalUrl: "https://www.arcgis.com",
+  oauthAppId: "HIER_OAUTH_APP_ID_EINTRAGEN",
+
+  // Basis-URL der editierbaren Trainer-Sicht, ohne Layer-ID.
+  // Beispiel:
+  // https://services-eu1.arcgis.com/.../arcgis/rest/services/Lagetraining_Daten_Trainer/FeatureServer
+  featureServiceUrl: "HIER_FEATURESERVICE_URL_EINTRAGEN",
+
+  layers: {
+    locations: 0,
+    vehicles: 10
+  },
+
+  fields: {
+    objectId: "OBJECTID",
+    globalId: "GlobalID",
+    vehicleHomeLocationId: "home_location_id",
+    locationType: "location_type"
+  },
+
+  // Bei Bedarf an die tatsächlich verwendeten Feldnamen anpassen.
+  labels: {
+    vehicle: ["radio_call_sign", "vehicle_name", "vehicle_code"],
+    location: ["location_name", "location_code"]
+  },
+
+  // Muss dem gespeicherten Domain-Code entsprechen.
+  fireStationTypeCode: "FEUERWACHE"
+};
