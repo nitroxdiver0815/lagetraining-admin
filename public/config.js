b3dbc5ee4c94dca3773072a329e0830dda30fd04
nitroxdiver0815 <nitroxdiver0815@gmail.com>
2026-09-20 14:33:17 +0200

    Erweitere Konfiguration für Szenario-Zuordnungen

--- public/config.js
+++ public/config.js
@@ -2,10 +2,16 @@ window.LAGETRAINING_ADMIN_CONFIG = {
   portalUrl: "https://www.arcgis.com",
   oauthAppId: "0sjHE1H45PUtL5k3",
   featureServiceUrl: "https://services-eu1.arcgis.com/XfUqDXJfAezaKUnU/arcgis/rest/services/Lagetraining_Daten_Trainer/FeatureServer",
+  abekServiceUrl: "https://services-eu1.arcgis.com/XfUqDXJfAezaKUnU/arcgis/rest/services/abek_catalog/FeatureServer",
 
   layers: {
     locations: 0,
-    vehicles: 10
+    vehicles: 10,
+    scenarioTemplates: 11,
+    scenarioLocations: 12,
+    scenarioVehicles: 13,
+    scenarioDispatch: 16,
+    abekCatalog: 0
   },
 
   fields: {
@@ -16,6 +22,7 @@ window.LAGETRAINING_ADMIN_CONFIG = {
   },
 
   labels: {
+    scenario: ["scenario_name", "scenario_code"],
     vehicle: ["radio_call_sign", "vehicle_name", "vehicle_code"],
     location: ["location_name", "location_code"]
   },
